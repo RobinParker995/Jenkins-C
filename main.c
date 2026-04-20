9a850ce8b6ac28092bc7b4fe09e979944bc16863
Robin Parker <157223424+MdAffan009@users.noreply.github.com>
2026-04-20 12:47:08 +0530

    Eliminated the problems from the code
    * Solved String Literal Problem
    
    * Solved Memory Leak Problem

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main()
 {
     char* name = malloc(30);
 
-    name = "Robin Parker";
+    strcpy(name, "Robin Parker");
 
     printf("I am %s \n", name);
 
     printf("This is Jenkins Templete for the agent \n");
+
+    free(name);
 
     return 0;
 }
